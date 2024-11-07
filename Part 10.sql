9cd4518d2f6f6314bcf177dc4a708f9c01e90a7e
pranish968 <164493799+pranish968@users.noreply.github.com>
2024-11-07 14:17:46 +0000

    Add files via upload

---
+++ Part 10.sql
@@ -0,0 +1,66 @@
+#07-11-2024
+create database empresa;
+use empresa;
+
+create table funcionarios(
+id_funcionario int,
+nome varchar(20),
+morada varchar(40) ,
+contacto int,
+primary key(id_funcionario)
+);
+
+
+create table clientes(id_cliente int auto_increment ,
+nome varchar(20),
+morada varchar(40) ,
+contacto int,
+id_funcionario int,
+primary key(id_cliente),
+foreign key(id_funcionario) references funcionarios(id_funcionario));
+
+select * from clientes,funcionarios;
+
+insert into funcionarios values 
+(1,'prince 1','figueira',920123456),
+(2,'prince 2','marinha das ondas ',123456778),
+(3,'prince 3','lisbon',435456547),
+(4,'prince 4','porto',658678679);
+
+insert into clientes values 
+(1,'celso 1','figueira',920123455,1),
+(2,'celso 2','marinha das ondas ',123456779,2),
+(3,'celso 3','lisbon',435456542,3),
+(4,'celso 4','porto',658678676,4);
+
+#inner join
+select funcionarios.nome, funcionarios.id_funcionario
+from funcionarios
+inner join clientes
+on funcionarios.id_funcionario = clientes.id_funcionario;
+
+#cross join 
+SELECT funcionarios.nome, funcionarios.id_funcionario
+FROM funcionarios
+CROSS JOIN clientes;
+
+#outer join 
+SELECT funcionarios.nome, funcionarios.id_funcionario
+FROM funcionarios
+LEFT JOIN clientes ON funcionarios.id_funcionario = clientes.id_funcionario
+UNION
+SELECT funcionarios.nome, funcionarios.id_funcionario
+FROM funcionarios
+RIGHT JOIN clientes ON funcionarios.id_funcionario = clientes.id_funcionario;
+
+
+#left join
+SELECT funcionarios.nome, funcionarios.id_funcionario
+FROM funcionarios
+LEFT JOIN clientes ON funcionarios.id_funcionario = clientes.id_funcionario;
+
+#right join 
+SELECT funcionarios.nome, funcionarios.id_funcionario
+FROM funcionarios
+RIGHT JOIN clientes ON funcionarios.id_funcionario = clientes.id_funcionario;
+
